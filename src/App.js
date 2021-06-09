0bf6c7e8f3f410c5a66473ae1698037467e87688
haneenbader <haneenalwatan993@gmail.com>
2021-06-10 01:17:52 +0300

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Main from './component/Main'
 import Header from './component/Header'
 import Footer from './component/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export class App extends Component {
   render() {
