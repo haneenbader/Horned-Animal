18a262ac4e6581d36e0eda62387d3dc7c6824ade
haneenbader <haneenalwatan993@gmail.com>
2021-06-10 04:47:45 +0300

    lab04

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Header from './component/Header'
 import Footer from './component/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HornedForm from './component/HornedForm'
+import Data from "./assest/Data.json"
 
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    // change condition in hornedform
+    this.state = {
+      Pictures: Data,
+      
+    }
+  }
+  
+  filterHornes = (e) => {
+    this.setState({Pictures: e })
+
+  }
   render() {
     return (
       <div>
         <Header />
-        <HornedForm />
-        <Main/>
+        <HornedForm newState={this.filterHornes} Pictures={Data} />
+        <Main dataAfterFilter ={this.props.newState}/> 
         <Footer />
       </div>
     )
--- src/component/HornedForm.js
+++ src/component/HornedForm.js
@@ -2,35 +2,53 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export class HornedForm extends Component {
-    render() {
-        return (
-            <div>
-                <Form style={{ width: '18rem' }} >
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>How Many Horns You Want To See ? </Form.Label>
-
-                        <Container className="p-3" >
-                            <Form.Control as="select" defaultValue="All">
-                            <option value="All">All</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="3">More than three</option>
-                            </Form.Control>
-                        </Container>
-
-                    </Form.Group>
-
-
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        )
-    }
+    hornReset = (event) => {
+        event.preventDefault();
+    
+   let selectNumber = Number(event.target.value);
+   
+if (selectNumber > 0) {
+    let beastArr = this.props.Pictures.filter(hornNumber => {
+        return (Number(hornNumber.horns) === selectNumber)
+       
+    });
+    this.props.newState(beastArr);
+    console.log(beastArr)
 }
+else { this.props.newState(this.props.Pictures); }
+};
 
+render() {
+    return (
+        <div>
+
+            <Form style={{ width: '18rem' }}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>How Many Horns You Want To See ? </Form.Label>
+
+                    <Container className="p-3" >
+                        <Form.Control as="select" onChange={this.hornReset}>
+                            <option value="0">All</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">More than three</option>
+                        </Form.Control>
+                    </Container>
+
+                </Form.Group>
+
+
+                <Button variant="primary" type="submit">
+                    Filter Horns
+                    </Button>
+            </Form>
+        </div>
+    )
+};
+}
 export default HornedForm
